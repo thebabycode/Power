d1ce34e2a193e72f4739ddebd09c5643d523ecc2
johnylogic <johns.baby@mca.christuniversity.in>
2022-08-10 23:29:21 +0530

    added buttons to the categories detail page

--- app/src/main/java/com/example/powersale/MainActivity2.java
+++ app/src/main/java/com/example/powersale/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.powersale;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 import android.content.Intent;
 
@@ -12,5 +14,16 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+        Button btn = findViewById(R.id.sunsetmainpage_btn);
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity2.this, activity_sunset_mainpage.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/powersale/activity_sunset_mainpage.java
@@ -0,0 +1,14 @@
+package com.example.powersale;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class activity_sunset_mainpage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sunset_mainpage);
+    }
+}
