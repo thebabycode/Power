d8ac00937644349f41735a630504daf2a8b1944a
johnylogic <johns.baby@mca.christuniversity.in>
2022-08-12 01:20:14 +0530

    added image button from main page and sunset main page to both pointing to the same product page

--- app/src/main/java/com/example/powersale/MainActivity2.java
+++ app/src/main/java/com/example/powersale/MainActivity2.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -17,10 +18,20 @@ public class MainActivity2 extends AppCompatActivity {
     }
 
     public void addListenerOnButton() {
-        Button btn = findViewById(R.id.sunsetmainpage_btn);
-        btn.setOnClickListener(new View.OnClickListener(){
+        Button sunsetmainpage_btn = findViewById(R.id.sunsetmainpage_btn);
+        ImageButton snst1_btn = findViewById(R.id.snst1_btn);
+
+        sunsetmainpage_btn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity2.this, activity_sunset_mainpage.class);
+                startActivity(i);
+            }
+        });
+
+        snst1_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity2.this, activity_snst1_page.class);
                 startActivity(i);
             }
         });
