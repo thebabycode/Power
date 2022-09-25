521cda18489671b5f8baa7260be4319359029e65
Johns Baby <johns.baby@mca.christuniversity.in>
2022-09-25 10:33:43 +0530

    Added Notification to Review Page

--- app/src/main/java/com/example/powersale/reviewpage.java
+++ app/src/main/java/com/example/powersale/reviewpage.java
@@ -1,6 +1,12 @@
 package com.example.powersale;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +16,7 @@ import android.widget.Toast;
 public class reviewpage extends AppCompatActivity {
 
     Button rate_btn;
+    Button submit_btn;
     RatingBar ratingStars;
     float rateResult =0;
 
@@ -20,6 +27,7 @@ public class reviewpage extends AppCompatActivity {
 
         rate_btn = findViewById(R.id.ratingbutton);
         ratingStars = findViewById(R.id.ratingbar);
+        submit_btn = findViewById(R.id.submitbutton);
 
         ratingStars.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
@@ -49,10 +57,31 @@ public class reviewpage extends AppCompatActivity {
                 Toast.makeText(reviewpage.this, msg, Toast.LENGTH_SHORT).show();
             }
         });
+
         rate_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(reviewpage.this, String.valueOf(rateResult), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("review notification","review notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        submit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(reviewpage.this,"review notification");
+                builder.setContentTitle("My Title");
+                builder.setContentText("Hello from Johns");
+                builder.setSmallIcon(R.drawable.iconimage);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(reviewpage.this);
+                managerCompat.notify(1, builder.build());
             }
         });
     }
